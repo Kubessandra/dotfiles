e90bf901bbdb3d3623d3505bd18c4edf6a1d582f
Kubessandra <aymeric.beudaert@gmail.com>
2024-02-19 14:19:36 +0100

    chore: add dotfiles
    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,11 +14,23 @@ function cd-perso
   cd ~/Documents/Personal_Aymeric_Beudaert/
 end
 
+function cd-tnv
+  cd ~/Documents/Personal_Aymeric_Beudaert/TNV/tnv-v2/
+end
+
 set -x PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
 set -x PUPPETEER_EXECUTABLE_PATH (which chromium)
 set -x ANDROID_SDK_ROOT /Users/aymeric/Library/Android/sdk
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home
 set PATH $PATH $ANDROID_SDK_ROOT/emulator
 set PATH $PATH $ANDROID_SDK_ROOT/platform-tools
+set PATH $PATH /Users/aymeric/Library/Python/3.11/bin
+set PATH $PATH ~/.bun/bin
+set PATH $PATH $HOME/.maestro/bin
 
 nvm use default
 
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
